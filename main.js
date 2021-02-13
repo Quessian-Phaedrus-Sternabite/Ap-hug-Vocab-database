94a6b44f743f8ed304836c0202292e217de5b8de
AsherSeiling <f14asher@gmail.com>
2021-02-13 17:14:37 -0600

    Basic backend added still in need of definitions

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ let definition = [
     "Solar radiation captured with photovoltaic cells that convert light energy to electrical energy.", 
     "The process of changes in culture that result from the meeting of two groups, each of which retains distinct culture features."
 ];
+
+var output = "";
+
 function main(){
-    
+    let term = $("#word_search").val();
+    let index1 = words.indexOf(term.toLowerCase());
+    output += "<p>" + term + ": " + definition[index1] + "</p>";
+    console.log(output);
+    $("#Output").html(output);
 };
