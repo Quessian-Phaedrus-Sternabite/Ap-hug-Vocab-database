8fc62d46b467cc9789523b5fa7cccc4faf3a4b66
AsherSeiling <f14asher@gmail.com>
2021-02-13 11:32:08 -0600

    JSON deletion and cleanup of HTML

---
+++ main.js
@@ -0,0 +1,9 @@
+let words = ["abiotic", "active solar energy", "acculturation"];
+let definition = [
+    "Composed of nonliving or inorganic matter.", 
+    "Solar radiation captured with photovoltaic cells that convert light energy to electrical energy.", 
+    "The process of changes in culture that result from the meeting of two groups, each of which retains distinct culture features."
+];
+function main(){
+    
+};
