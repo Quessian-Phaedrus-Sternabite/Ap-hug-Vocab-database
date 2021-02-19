a4e6c725a83f0728f6386a589ca04f8b9549f82f
Asher Seiling <f14asher@gmail.com>
2021-02-18 20:50:13 -0600

    Addition of some missing Vocab words, as well as the addition of the 'Display all' words feature.

---
+++ dispAll.js
@@ -0,0 +1,30 @@
+// Load words.txt to an Array
+let words = new Array();
+$.get("words.txt", function(data){
+        words = data.split("\n");
+    });
+
+// Load deffinition.txt into an array
+let definition = new Array();
+$.get("deffinitions.txt", function(data){
+        definition = data.split("\n");
+    });
+
+function main(){
+    // The array of all finished data for the glossory
+    let finished_array = [];
+
+    for(let i in words){
+        finished_array.push(words[i] + " - " + definition[i]);
+    };
+    // Sort the main data storage array
+    finished_array.sort();
+
+    let buffer = "";
+
+    for(let i in finished_array){
+        buffer += "<p>" + finished_array[i] + "</p>";
+    };
+
+    $("#Display").html(buffer);
+};
