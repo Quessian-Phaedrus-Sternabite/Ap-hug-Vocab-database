b29aa4a7cf08f93c06492072bb0f811e860c49c5
Asher Seiling <f14asher@gmail.com>
2021-02-23 16:48:45 -0600

    Make search more presice and make stacking the results a togalable value.

--- main.js
+++ main.js
@@ -10,19 +10,57 @@ let definition = new Array();
             definition = data.split("\n");
     });
 
+// Variables needed for the global operation
+let output_buffer = "";
+let stacking = false;
+
+// Toggle stacking function
+function toggle_stacking(){
+    if (stacking == false){
+        stacking = true;
+    }
+    else{
+        stacking = false;
+    };
+};
 
 // Main function, executed on button press
 function main(){
     // Get word in the input box
     let term = $("#word_search").val();
-    if (term[term.length] == " "){
-        term -= " "
-    }
+    // Take spaces out of term
+    let term_search = "";
+    for (i in term){
+        if (term[i] != " "){
+            term_search += term[i];
+        };
+    };
     // Get the words index
-    let index1 = words.indexOf(term.toLowerCase());
+    let index1 = words.indexOf(term_search.toLowerCase());
     // Adds the definition and the term to the ouput
-    output = "<p>" + term + " - " + definition[index1] + "</p>";
+    // Check stacking
+    if (stacking == false){
+        output = "<p>" + term + " - " + definition[index1] + "</p>";
+        output_buffer = term + " - " + definition[index1];
+    }
+    else{
+        output += "<p>" + term + " - " + definition[index1] + "</p>";
+        output_buffer += term + " - " + definition[index1];
+    };
+    term_search = "";
     // Ouputs the output
     $("#Output").html(output);
 };
 
+function copy_text() {
+    let dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = output_buffer;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
