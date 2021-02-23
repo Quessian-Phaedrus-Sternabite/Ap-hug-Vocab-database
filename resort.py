1fc385ef7e328ad7ff53b11e9df662d731a4ec4d
Asher Seiling <f14asher@gmail.com>
2021-02-23 17:34:38 -0600

    Resorted all of the words for optimum effecency

--- resort.py
+++ resort.py
@@ -1,4 +1,31 @@
 words = open("words.txt", "r")
 deffinitions = open("deffinitions.txt", "r")
-words = words.readlines()
-print(words)
+words = [x.rstrip("\n") for x in words.readlines()]
+deffinitions = [x.rstrip("\n") for x in deffinitions.readlines()]
+
+deffinitions_ref = []
+deffinitions_indexed = []
+words_sorted = []
+for i in words:
+    words_sorted.append(i)
+
+for i in words:
+    deffinitions_ref.append(words.index(i))
+
+words.sort()
+for i in words:
+    deffinitions_indexed.append(words_sorted.index(i))
+
+
+finished_def = []
+finished_word = []
+
+for i in deffinitions_indexed:
+    finished_word.append(words_sorted[i])
+    finished_def.append(deffinitions[i])
+
+for i in finished_word:
+    print(i)
+print("\n\n\n")
+for i in finished_def:
+    print(i)
