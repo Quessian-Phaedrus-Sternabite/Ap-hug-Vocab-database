c7330aeb63d4ca543592f7696903ff8f310585f6
Quessian-Phaedrus-Sternabite <56981027+Quessian-Phaedrus-Sternabite@users.noreply.github.com>
2021-02-27 11:58:36 -0600

    Fixed spelling of definition from deffinition.

--- automate_conversion.py
+++ automate_conversion.py
@@ -3,13 +3,13 @@
 text_file = open("glossory.txt", "r")
 glossory = text_file.readlines()
 word = []
-deffinition = []
+definition = []
 
 for i in range(len(glossory)):
     if (i % 2) == 0:
         word.append(glossory[i])
     else:
-        deffinition.append(glossory[i])
+        definition.append(glossory[i])
 
 word[:] = [line.rstrip('\n') for line in word]
 deffinition[:] = [line.rstrip('\n') for line in deffinition]
